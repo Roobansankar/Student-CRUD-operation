a6f7960b8054062f71d1126e032133a19b356a5f
Rooban sankar <146194005+Roobansankar@users.noreply.github.com>
2024-01-01 20:20:33 +0530

    Update Main.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import("../App.css")
+
 
 const Main = () => {
   const [records, setRecords] = useState([]);
@@ -12,7 +12,6 @@ const Main = () => {
       .then(res => setRecords(res.data))
       .catch(err => console.log(err));
   }, []);
-
 
   function handleDelete(id) {
     const conf = window.confirm('Do you want to delete?');
@@ -28,66 +27,41 @@ const Main = () => {
 
   return (
     <div className='container mt-5'>
-      <h1 style={{marginLeft:"350px"}}> STUDENT MANAGEMENT SYSTEM</h1>
+      <h1 className='text-center'>STUDENT MANAGEMENT SYSTEM</h1>
       <div className='text-end'>
         <Link to='/create' className='btn btn-primary'>Add+</Link>
       </div>
-      <table className='table'>
-        <thead>
-          <tr>
-            {/* <th>ID</th> */}
-            <th>Name</th>
-            <th>Age</th>
-            <th id='city'>City</th>
-            <th className='actions' id="act">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map((d, i) => (
-            <tr key={i}>
-              {/* <td>{d._id}</td> */}
-              <td>{d.Name}</td>
-              <td>{d.Age}</td>
-              <td>{d.City}</td>
-              <td>
-              <Link to={`/update/${d._id}`} className='btn btn-sm btn-success'>
-          Update
-        </Link>
-                <button onClick={() => handleDelete(d._id)} className='btn btn-sm btn-danger'>
-                  Delete
-                </button>
-              </td>
-              {/* <td>
-                <button onClick={() => handleDelete(d._id)} className='btn btn-sm btn-danger'>
-                  Delete
-                </button>
-              </td> */}
+      <div className='table-responsive'>
+        <table className='table table-striped table-hover'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>City</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {records.map((d, i) => (
+              <tr key={i}>
+                <td>{d.Name}</td>
+                <td>{d.Age}</td>
+                <td>{d.City}</td>
+                <td>
+                  <Link to={`/update/${d._id}`} className='btn btn-success me-2'>
+                    Update
+                  </Link>
+                  <button onClick={() => handleDelete(d._id)} className='btn btn-danger'>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
 export default Main;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
